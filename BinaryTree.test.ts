54fb09e391a64be66de97841f7c32f5657bafd4b
Sam Darbonne <samdarbonne@gmail.com>
2024-09-03 17:35:41 +0000

    completes AVL tree

--- BinaryTree.test.ts
+++ BinaryTree.test.ts
@@ -1,36 +1,99 @@
-import BinaryTree from "./BinaryTree";
+import BinaryTree, { BinaryTreeNode } from "./BinaryTree";
+
+const testSorting = (node): boolean => {
+    if (node.left !== null) {
+        expect(node.compare(node.left.value)).toBe(-1)
+        if (!testSorting(node.left)) return false
+    }
+    if (node.right !== null) {
+        expect(node.compare(node.right.value)).toBe(1)
+        if (!testSorting(node.right)) return false
+    }
+    return true;
+}
 
 describe('Binary Tree', () => {
     describe('should instantiate', () => {
-        it('with no list', () => {
-            const tree = new BinaryTree([]);
-            expect(tree.root?.value).toBe(undefined)
-            expect(tree.root?.left).toBe(null)
-            expect(tree.root?.right).toBe(null)
+        describe('with one input', () => {
+            it('number', () => {
+                const tree = new BinaryTree<number>(1);
+                expect(tree.root.value).toBe(1)
+                expect(tree.root.left).toBe(null)
+                expect(tree.root.right).toBe(null)
+            })
+            it('string', () => {
+                const tree = new BinaryTree<string>('hello');
+                expect(tree.root.value).toBe('hello')
+                expect(tree.root.left).toBe(null)
+                expect(tree.root.right).toBe(null)
+            })
         })
-        it('with a list', () => {
-            const tree = new BinaryTree([1,2,3,4]);
-            expect(tree.root?.value).toBe(2)
-            expect(tree.root?.right?.right?.value).toBe(4)
+        describe('with a list', () => {
+            describe('of numbers', () => {
+                it('sorted', () => {
+                    const tree = new BinaryTree<number>([1,2,3,4]);
+                    expect(testSorting(tree.root)).toBe(true);
+                })
+                it('unsorted', () =>{
+                    const tree = new BinaryTree([1,4,3,2]);
+                    expect(testSorting(tree.root)).toBe(true);
+                })
+            })
+
+            describe('of strings', () => {
+                it('sorted', () => {
+                    const tree = new BinaryTree(['a', 'b', 'c', 'd']);
+                    expect(testSorting(tree.root)).toBe(true)
+                })
+                it('unsorted', () =>{
+                    const tree = new BinaryTree(['a', 'd', 'c', 'b']);
+                    expect(testSorting(tree.root)).toBe(true)
+                })
+            })
         })
     })
-    describe('should sort inputs properly', () => {
-        it('with numbers', () => {
-            const tree = new BinaryTree([1,4,5,6,3,2,9])
-            expect(tree.root?.value).toBe(4);
-            expect(tree.root?.left?.left?.value).toBe(1);
-            expect(tree.root?.right?.right?.value).toBe(9);
+    describe('should calculate height correctly', () => {
+        it('for lopsided insertion', () => {
+            const tree = new BinaryTree<number>([4])
+            tree.insert(3)
+            tree.insert(2)
+            expect(tree.root.getHeight()).toBe(2)
+        })
+        it('for balanced trees', () => {
+            const tree = new BinaryTree<number>([4,2,6,1,3,5,7])
+            expect(tree.root.getHeight()).toBe(3)
         })
     })
-    describe('should insert properly', () => {
-        it('with numbers', () => {
-            const tree1 = new BinaryTree<number>([1,2,3,4,5,6,7])
-            tree1.insert(8)
-            expect(tree1.root?.right?.right?.right?.value).toBe(8)
-            tree1.insert(3)
-            expect(tree1.root?.left?.right?.left?.value).toBe(3)
-            const tree2 = new BinaryTree([1,1,1,2,3,4,5])
-            expect(tree2.root?.left?.left?.value).toBe(1)
+    describe('should rotate', () => {
+        describe('right', () => {
+            it('simple tree', () => {
+                let root = new BinaryTreeNode<number>(2);
+                root.left = new BinaryTreeNode<number>(1);
+                root.right = new BinaryTreeNode<number>(3);
+                root = root.rotateRight();
+                expect(root.value).toBe(1)
+                expect(root.left).toBe(null);
+                expect(root.right?.value).toBe(2)
+                expect(root.right?.right?.value).toBe(3)
+                expect(root.height).toBe(3);
+                expect(root.right?.height).toBe(2)
+                expect(root.right?.right?.height).toBe(1)
+            })
+        })
+        describe('left', () => {
+            it('simple tree', () => {
+                let root = new BinaryTreeNode<number>(2);
+                root.left = new BinaryTreeNode<number>(1);
+                root.right = new BinaryTreeNode<number>(3);
+                root = root.rotateLeft();
+                expect(root.value).toBe(3);
+                expect(root.right).toBe(null);
+                expect(root.left?.value).toBe(2);
+                expect(root.left?.left?.value).toBe(1);
+                expect(root.height).toBe(3);
+                expect(root.left?.height).toBe(2);
+                expect(root.left?.left?.height).toBe(1);
+            })
         })
     })
 })
--- BinaryTree.ts
+++ BinaryTree.ts
@@ -1,105 +1,121 @@
-class BinaryTreeNode<T> {
+export class BinaryTreeNode<T> {
     value: T;
-    left?: BinaryTreeNode<T> | null;
-    right?: BinaryTreeNode<T> | null;
-    constructor(value: T, left?: BinaryTreeNode<T> | null, right?: BinaryTreeNode<T> | null) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
-}
-
-type InputList<T> = [T, ...T[]]
-
-class BinaryTree<Entry> {
-    root: BinaryTreeNode<Entry> | null;
-    priority?: (arg0: Entry) => number;
-    buildTree: (inputList: Entry[]) => BinaryTreeNode<Entry>
-    constructor(list: Entry[], priority?: (arg0: Entry) => number) {
+    left: BinaryTreeNode<T> | null = null;
+    right: BinaryTreeNode<T> | null = null;
+    height: number;
+    priority?: (value: T) => number;
+    constructor(value: T, priority?) {
+        this.height = 1;
         this.priority = priority;
-        this.buildTree = (inputList: Entry[]) => {
-            if (inputList.length === 1) return new BinaryTreeNode<Entry>(inputList[0]);
-            if (priority !== undefined) {
-                inputList.sort((a, b) => priority(a) - priority(b))
-            } else {
-                inputList.sort()
-            }
-            const mid = Math.floor((inputList.length - 1) / 2);
-            const leftArray: Entry[] = [...inputList].splice(0, mid);
-            const rightArray: Entry[] = [...inputList].splice(mid + 1, inputList.length);
-            const left = leftArray.length ? this.buildTree(leftArray as InputList<Entry>) : null;
-            const right = rightArray.length ? this.buildTree(rightArray as InputList<Entry>) : null;
-            const newNode: BinaryTreeNode<Entry> = new BinaryTreeNode<Entry>(inputList[mid], left, right);
-            return newNode;
-        }
-        this.root = this.buildTree(list)
+        this.value = value;
     }
 
-    getHeight(node: BinaryTreeNode<Entry>): number {
-        if (!node) return 0
-        const {left, right} = node;
-        if (!left && !right) return 0;
-        else if (right && !left) return this.getHeight(right) + 1;
-        else if (left && !right) return this.getHeight(left) + 1;
-        else if (right && left) return Math.max(this.getHeight(left), this.getHeight(right)) + 1
-        return 0
+    getHeight(node: BinaryTreeNode<T> | null = this): number {
+        if (!node) return 0;
+        if (!node.left && !node.right) return 1;
+        return Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
     }
 
-    isBalanced(node: BinaryTreeNode<Entry>): boolean {
-        const left = node.left ? this.getHeight(node.left) : 0;
-        const right = node.right ? this.getHeight(node.right) : 0;
-        if (Math.abs(left - right) < 2) {
-            if (node.left && node.right) {
-                return this.isBalanced(node.left) && this.isBalanced(node.right)
-            } else if (node.left) return this.isBalanced(node.left)
-            else if (node.right) return this.isBalanced(node.right)
-            else return true
-        }
-        return false
+    getBalance(node: BinaryTreeNode<T> = this): number {
+        return ((node.left?.getHeight() || 0) - (node.right?.getHeight() || 0));
     }
 
-    compare(valueA: Entry, valueB: Entry): 0 | 1 | -1 {
+    compare(nodeA: T, nodeB: T = this.value): 0 | 1 | -1 {
         let a, b;
         if (this.priority) {
-            [a, b] = [this.priority(valueA), this.priority(valueB)]
-        } else [a, b] = [valueA, valueB];
+            [a, b] = [this.priority(nodeA), this.priority(nodeB)]
+        } else [a, b] = [nodeA, nodeB];
         if (a === b) return 0;
-        if (a < b) return 1;
-        return -1
+        if (a < b) return -1;
+        return 1
     }
 
-    insert(entryValue: Entry, root = this.root): void {
-        if (!root?.value) {
-            this.root = new BinaryTreeNode<Entry>(entryValue)
-            return
+    rotateRight(node: BinaryTreeNode<T> = this): BinaryTreeNode<T> {
+        if (node.left) {
+            const newNode = node.left;
+            const temp = newNode.right;
+            newNode.right = node;
+            newNode.right.left = temp;
+            newNode.height = newNode.getHeight();
+            newNode.right.height = newNode.right.getHeight();
+            if (newNode.left) newNode.left.height = newNode.left.getHeight();
+            return newNode
         }
-        const newNode = new BinaryTreeNode<Entry>(entryValue);
-        const comparison = this.compare(entryValue, root.value)
-        switch(comparison) {
+        return node
+    }
+
+    rotateLeft(node: BinaryTreeNode<T> = this): BinaryTreeNode<T> {
+        if (node.right) {
+            const newNode = node.right;
+            const temp = newNode.left;
+            newNode.left = node;
+            newNode.left.right = temp;
+            newNode.height = newNode.getHeight();
+            newNode.left.height = newNode.left.getHeight();
+            if (newNode.right) newNode.right.height = newNode.right.getHeight();
+            return newNode
+        }
+        return node
+    }
+
+    insert(entry: T, root: BinaryTreeNode<T> = this): BinaryTreeNode<T> {
+        if (!root?.value) {
+            root = new BinaryTreeNode<T>(entry)
+            return root
+        }
+        const comparison = this.compare(entry, root.value)
+        switch (comparison) {
+            case -1:
+                if (root.left) {
+                    root.left = root.left.insert(entry);
+                } else {
+                    root.left = new BinaryTreeNode<T>(entry, this.priority);
+                }
+                break;
             case 1:
-                if (root.left) this.insert(entryValue, root.left)
-                else root.left = newNode
+                if (root.right) {
+                    root.right = root.right.insert(entry);
+                } else {
+                    root.right = new BinaryTreeNode<T>(entry, this.priority);
+                }
                 break;
             case 0:
-                if (root.left) {
-                    newNode.left = root.left;
-                    root.left = newNode;
-                } else root.left = newNode;
+                return root;
                 break;
-            case -1:
-                if (root.right) this.insert(entryValue, root.right)
-                else root.right = new BinaryTreeNode<Entry>(entryValue)
         }
+        const balance = root.getBalance();
+            // Left-Left Case
+        if (balance > 1) {
+            if (this.compare(entry, root.left!.value)) {
+                root.left = root.left!.rotateLeft();
+                return root.rotateRight();
+            } return root.rotateRight()
+        } else if (balance < -1) {
+            if (this.compare(entry, root.right!.value)) {
+                root.right = root.right!.rotateRight();
+                return root.rotateLeft();
+            } return root.rotateLeft();
+        }
+        return root
     }
 }
 
-// const tree = new BinaryTree<number>([1,2,3,4,7,8])
-// console.log(tree.root)
-// if (tree.root?.value) {
-//     console.log(tree.getHeight(tree.root))
-//     console.log(tree.isBalanced(tree.root))
-//     tree.insert(5)
-//     console.log(tree.root)
-// }
+class BinaryTree<Entry> {
+    root: BinaryTreeNode<Entry>;
+    priority?: (value: Entry) => number;
+    constructor(value: Entry | Entry[], priority?) {
+    this.priority = priority;
+        if (Array.isArray(value)) {
+            this.root = new BinaryTreeNode<Entry>(value[0]);
+            value.slice(1).forEach(entry => this.insert(entry))
+        } else {
+            this.root = new BinaryTreeNode<Entry>(value)
+        }
+    }
+    insert(value: Entry): Entry {
+        this.root = this.root.insert(value)
+        return value;
+    }
+}
 
-export default BinaryTree;
+export default BinaryTree
